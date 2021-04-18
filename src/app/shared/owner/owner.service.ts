b3eccbc43ec2944d5178eb4e57bc52bbd413dd57
Joan sebastian gomez acevedo <joan.gomez@udea.edu.co>
2021-04-18 15:58:35 -0500

    add services owner

---
+++ src/app/shared/owner/owner.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OwnerService {
+  public API = '//thawing-chamber-47973.herokuapp.com';
+  public OWNERS_API = this.API + '/owners';
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<any> {
+    return this.http.get(this.OWNERS_API);
+  }
+
+  getByDni(dni: string) {
+    return this.http.get(this.OWNERS_API + '/findByDni?dni=' + dni);
+  }
+
+  post(owner: any) {
+    return this.http.post(this.OWNERS_API, owner);
+  }
+
+  put(url: string, owner: any) {
+    return this.http.put(url, owner);
+  }
+
+  delete(url: string, ) {
+    return this.http.delete(url);
+  }
+
+}
