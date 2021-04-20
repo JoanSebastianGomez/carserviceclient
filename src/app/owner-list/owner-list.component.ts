9449d0353d48a849e871e69dc182e3dc4024456e
Joan sebastian gomez acevedo <joan.gomez@udea.edu.co>
2021-04-19 22:05:07 -0500

    delete multiples owners

--- src/app/owner-list/owner-list.component.ts
+++ src/app/owner-list/owner-list.component.ts
@@ -8,14 +8,31 @@ import { OwnerService } from '../shared/owner/owner.service';
 })
 export class OwnerListComponent implements OnInit {
   owners: Array<any>;
+  toDelete: Array<string> = [];
   constructor(private ownerService: OwnerService) { }
 
   ngOnInit() {
     this.ownerService.getAll().subscribe(data => {
-     
       this.owners = data._embedded.owners;
     }
     )
   }
 
+  addToDelete(href) {
+    if (this.toDelete.includes(href)) {
+      this.toDelete.splice(this.toDelete.indexOf(href), 1)
+    } else {
+      this.toDelete.push(href)
+    }
+  }
+
+  delete() {
+    for (const toDeleteOwner in this.toDelete) {
+      this.ownerService.delete(this.toDelete[toDeleteOwner]).subscribe(result => {
+        this.ownerService.getAll().subscribe(data => {
+          this.owners = data._embedded.owners;
+        })
+      }, error => console.error(error))
+    }
+  }
 }
